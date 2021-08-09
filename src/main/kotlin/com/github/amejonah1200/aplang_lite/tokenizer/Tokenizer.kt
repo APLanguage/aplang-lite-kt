00644f56ee9402baf6446cba1d266f2d3722c500
Amejonah1200 <amejonah@gmail.com>
2021-08-09 22:00:40 +0200

    Implementing Parser: matching Numbers and chars

--- src/main/kotlin/com/github/amejonah1200/aplang_lite/tokenizer/Tokenizer.kt
+++ src/main/kotlin/com/github/amejonah1200/aplang_lite/tokenizer/Tokenizer.kt
@@ -1,15 +1,21 @@
 package com.github.amejonah1200.aplang_lite.tokenizer
 
 import com.github.amejonah1200.aplang_lite.utils.CharScanner
+import java.math.BigInteger
 import java.util.*
 
-data class ScanResult(val tokens: List<Token>)
+enum class ScanErrorType {
+  NUMBER_FORMAT
+}
+
+data class ScanResult(val tokens: List<Token>, val liteErrors: List<ScanErrorType>)
 
 class ParserException(msg: String) : RuntimeException(msg)
 
 fun scan(scanner: CharScanner): ScanResult {
   scanner.reset()
   val list = mutableListOf<Token>()
+  val errors = mutableListOf<ScanErrorType>()
   var posX = 0
   var posY = 0
   var buffer: Optional<String>
@@ -23,29 +29,63 @@ fun scan(scanner: CharScanner): ScanResult {
       posY += 1
       continue
     }
-    buffer = scanner.consumeChars(LONGEST_TOKEN_LENGTH, false)
+    buffer = scanner.peekChars(LONGEST_TOKEN_LENGTH, false)
     if (buffer.isEmpty) break
     else if (buffer.get()[0].isWhitespace()) continue
     else if (buffer.get()[0].isDigit()) {
-      //parse Digit
+      scanner.resetPeek()
+      if (scanner.peekSearch("0x")) {
+        scanner.advancePosition(2)
+        val nb = scanner.searchConsumeChars {
+          it.isDigit() || it in 'A'..'F' || it in 'a'..'f'
+        }
+        if (nb.isEmpty()) {
+          list.add(Token.IntegerToken(BigInteger.ZERO))
+          scanner.rewindPosition(1)
+        } else {
+          list.add(Token.IntegerToken(BigInteger(nb, 16)))
+        }
+      } else if (scanner.peekSearch("0b")) {
+        scanner.advancePosition(2)
+        val nb = scanner.searchConsumeChars(Char::isDigit)
+        if (nb.isEmpty()) {
+          list.add(Token.IntegerToken(BigInteger.ZERO))
+          scanner.rewindPosition(1)
+        } else {
+          val bi = nb.toBigIntegerOrNull(2)
+          if (bi == null) {
+            list.add(Token.IntegerToken(BigInteger(nb)))
+            errors.add(ScanErrorType.NUMBER_FORMAT)
+          } else list.add(Token.IntegerToken(bi))
+        }
+      } else {
+        val nb = scanner.searchConsumeChars(Char::isDigit)
+        if (scanner.peekSearchChar('.') && scanner.peekSearchWithPredicate(Char::isDigit)) {
+          scanner.advancePosition(1)
+          list.add(Token.FloatToken(BigInteger(nb), BigInteger(scanner.searchConsumeChars(Char::isDigit))))
+        } else list.add(Token.IntegerToken(BigInteger(nb)))
+      }
+      continue
     }
     when (buffer.get()[0]) {
       '"' -> {
-        scanner.rewindPosition(buffer.get().length)
         val match = Regex("\".*?\"(?<!\\\\\")").find(scanner.str, scanner.position)
           ?: throw ParserException("No closing quoting mark for string at $posY:$posX")
         list.add(Token.StringToken(match.value.substring(1, match.value.length - 1)))
         scanner.advancePosition(match.value.length)
       }
       '\'' -> {
-        scanner.rewindPosition(buffer.get().length)
-
+        val match = Regex("'(?:\\\\[^\\\\]|.)'").find(scanner.str, scanner.position)
+          ?: throw ParserException("No closing apostrophe mark for string at $posY:$posX")
+        if (match.range.first != scanner.position) throw ParserException("No closing apostrophe mark for string at $posY:$posX")
+        list.add(Token.CharToken(match.value.substring(1, match.value.length - 1)))
+        scanner.advancePosition(match.value.length)
       }
       else -> {
         val tkOpt = parseToken(buffer.get())
         tkOpt.ifPresentOrElse(
           {
-            scanner.rewindPosition(LONGEST_TOKEN_LENGTH - it.length())
+            scanner.advancePosition(LONGEST_TOKEN_LENGTH)
             posX += it.length()
             if (it is Token.SignToken && it.codeToken == CodeToken.SLASH_SLASH) {
               scanner.searchConsumeChars { chr -> chr != '\n' }
@@ -58,6 +98,6 @@ fun scan(scanner: CharScanner): ScanResult {
       }
     }
   }
-  return ScanResult(list)
+  return ScanResult(list, errors)
 }
 
