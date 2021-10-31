caeae519cc4f2b3a684e8455c7ad61e0b1b70f4c
Amejonah1200 <amejonah@gmail.com>
2021-10-31 16:31:57 +0100

    Add some (inline) utility methods

--- src/main/kotlin/com/github/aplanguage/aplanglite/utils/Utils.kt
+++ src/main/kotlin/com/github/aplanguage/aplanglite/utils/Utils.kt
@@ -1,5 +1,7 @@
 package com.github.aplanguage.aplanglite.utils
 
+import java.nio.ByteBuffer
+
 inline fun <T> listOfUntilNull(generator: () -> T?): List<T> {
   var temp: T? = generator()
   val list = mutableListOf<T>()
@@ -8,4 +10,34 @@ inline fun <T> listOfUntilNull(generator: () -> T?): List<T> {
     temp = generator()
   }
   return list.toList()
+}
+
+infix fun Int.nextBit(b: Boolean) = this shl 1 or if (b) 1 else 0
+operator fun Boolean.plus(other: Boolean) = if (this) (if (other) 2 else 1) else if (other) 1 else 0
+operator fun Int.plus(b: Boolean) = if (b) this + 1 else this
+operator fun ByteBuffer.plus(other: ByteBuffer): ByteBuffer = ByteBuffer.allocate(limit() + other.limit()).put(this).put(other)
+operator fun ByteBufferable.plus(other: ByteBufferable): ByteBuffer = toByteBuffer() + other.toByteBuffer()
+inline fun ByteBuffer.put(uByte: UByte): ByteBuffer = put(uByte.toByte())
+inline fun ByteBuffer.put(i: Int): ByteBuffer = put(i.toByte())
+inline fun ByteBuffer.put(i: UInt): ByteBuffer = put(i.toByte())
+inline fun ByteBuffer.putShort(uShort: UShort): ByteBuffer = putShort(uShort.toShort())
+inline fun ByteBuffer.putShort(i: Int): ByteBuffer = putShort(i.toShort())
+inline fun ByteBuffer.putInt(uInt: UInt): ByteBuffer = putInt(uInt.toInt())
+inline fun ByteBuffer.putLong(uLong: ULong): ByteBuffer = putLong(uLong.toLong())
+inline fun <T> ByteBuffer.putCollection(byteSize: Int, collection: Collection<T>, putFunc: (ByteBuffer, T) -> Unit) : ByteBuffer{
+  when(byteSize) {
+    1 -> put(collection.size.toByte())
+    2 -> putShort(collection.size.toShort())
+    4 -> putInt(collection.size)
+    else -> throw IllegalArgumentException("Only byteSize of 1, 2 or 4 allowed, got $byteSize!")
+  }
+  for(item in collection) {
+    putFunc(this, item)
+  }
+  return this
+}
+fun ByteArray.toByteBuffer(): ByteBuffer = ByteBuffer.allocate(size).put(this)
+
+interface ByteBufferable {
+  fun toByteBuffer(): ByteBuffer
 }
